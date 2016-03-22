a7edde7902550c6f275db16913f7aae3fbf689b4
Masamine Doko <pencil.do@gmail.com>
2016-03-22 09:58:20 +0900

    Arduino化＆Wi-Fi接続

--- ESP-WROOM-02/ESP-WROOM-02.ino
+++ ESP-WROOM-02/ESP-WROOM-02.ino
@@ -3,7 +3,7 @@ ESP-WROOM-02
 
 https://www.mgo-tec.com/blog-entry-ss-wroom-howto01.html
 を参考にArduino化＆Wi-Fi接続。
-※setup()をloop()より前に持ってこないとエラーが出る
+※Ini_HTTP_Response()をloop()より前に持ってこないとエラーが出る
 
 ブラウザを更新するたびにLEDが点滅する仕様を追加。
 →takeLight
